2275af499163cbb30baa313e03cc7c8ad887f89a
bbaa30 <84327617+bbaa30@users.noreply.github.com>
2023-02-21 12:04:09 +0100

    Fix in install_packages.sh

--- bin/install_packages.sh
+++ bin/install_packages.sh
@@ -1,8 +1,8 @@
-# Install packages in conda using this sh script
-
-# Add the conda-forge channel and the hallkjc01 channel (xcast)
+## Install packages in conda using this sh script
+#
+# Add the conda-forge channel
 conda config --add channels conda-forge
 conda config --add channels hallkjc01
-
+#
 # Install from file
-while read requirement; do conda install --yes $requirement; done < requirements.txt
+while read requirements; do conda install --yes $requirements; done < requirements.txt
